a78eb118963aed7a075c019d1530a7063ffd8f4d
kunwardeeps <kunwardeep.singh@gmail.com>
2018-05-16 19:06:37 -0400

    Now can run on VSCode

--- src/com/leetcode/solutions/Problem1TwoSum.java
+++ src/com/leetcode/solutions/Problem1TwoSum.java
@@ -15,6 +15,46 @@ public class Problem1TwoSum {
 
     public static void main(String[] args) {
 
+        int[] input = new int[]{2, 7, 11, 15};
+        printArray(input);
+        int[] output = naiveSolution(input, 9);
+        printArray(output);
+
+    }
+
+    private static int[] solution1(int[] input, int target) {
+        int[] out = new int[2];
+        return out;
+    }
+
+    private static int[] solution2(int[] input, int target) {
+        int[] out = new int[2];
+        return out;
+    }
+
+    private static int[] naiveSolution(int[] input, int target) {
+        int j = 0;
+        int[] out = new int[2];
+        for (int i=0; i<input.length; i++){
+            for (j=i+1; j<input.length; j++){
+                if (input[i] + input[j] == target){
+                    out[0] = i;
+                    out[1] = j;
+                    return out;
+                }
+            }
+        }
+        return out;
+    }
+
+    private static void printArray(int[] arr){
+        StringBuilder sb = new StringBuilder();
+        for (int x: arr){
+            sb.append(x);
+            sb.append(",");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb.toString());
     }
 
 }
