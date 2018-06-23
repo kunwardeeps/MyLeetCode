c01eb6fbda7139ad2f567aaa93f45ba43f00563e
kunwardeeps <kunwardeep.singh@gmail.com>
2018-06-23 14:27:10 -0400

    Contest attemp

---
+++ src/com/leetcode/solutions/Problem6.java
@@ -0,0 +1,40 @@
+package com.leetcode.solutions;
+
+class Problem6{
+    public static void main(String[] args) {
+        System.out.println(solution1("PAYPALISHIRING", 2).equals("PYAIHRNAPLSIIG"));
+        System.out.println(solution1("PAYPALISHIRING", 3).equals("PAHNAPLSIIGYIR"));
+        System.out.println(solution1("PAYPALISHIRING", 4).equals("PINALSIGYAHRPI"));
+    }
+
+    private static String solution1(String s, int numRows){
+        StringBuilder str = new StringBuilder();
+        int x=0;
+        if (numRows==1){
+            return s;
+        }
+        if (numRows==2){
+            for (int i=0; i<s.length(); i++){
+                if (i%2==0){
+                    str.append(s.charAt(i));
+                }
+            }
+            for (int i=0; i<s.length(); i++){
+                if (i%2!=0){
+                    str.append(s.charAt(i));
+                }
+            }
+            return str.toString();
+        }
+        for (int r=0; r<numRows; r++){
+            for (int i=r; i<s.length(); i+=2*numRows-2){
+                str.append(s.charAt(i));
+                x = i+2*(numRows-r-1);
+                if (r!=0 && r!=numRows-1 && x<s.length()){
+                    str.append(s.charAt(x));
+                }
+            }
+        }
+        return str.toString();
+    }
+}
